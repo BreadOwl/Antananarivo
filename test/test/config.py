53d0df33b9c03380a289899f8831b309a3f31a42
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2022-12-23 23:06:52 +0300

    Delete test directory

--- test/test/config.py
+++
@@ -1,8 +0,0 @@
-import os
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'mysql://admin:top@127.0.0.1:6667/database.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- test/test/routes.py
+++
@@ -1,8 +0,0 @@
-from test import app
-from test.models import Book
-from flask import render_template, send_from_directory
-
-@app.route('/')
-def index():
-    books = Book.query.all()
-    return render_template('index.html', books=books)
