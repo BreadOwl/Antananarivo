a16e62c7e0eea4ed22c03eece732b1af0649e2f8
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2022-12-23 22:39:40 +0300

    Add files via upload

--- app2/app/app.py
+++ app2/app/app.py
@@ -2,6 +2,9 @@ from typing import List, Dict
 from flask import Flask
 import mysql.connector
 import json
+from flask_migrate import Migrate
+from app.models import Book
+migrate = Migrate(app, db)
 
 app = Flask(__name__)
 
@@ -24,10 +27,16 @@ def test_table() -> List[Dict]:
     return results
 
 
-@app.route('/')
-def index() -> str:
-    return json.dumps({'test_table': test_table()})
+#@app.route('/')
+#def index() -> str:
+ #   return json.dumps({'test_table': test_table()})
 
+@app.route('/')
+def index():
+    books = Book.query.all()
+    return render_template('index.html', books=books)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+from app import routes, models
