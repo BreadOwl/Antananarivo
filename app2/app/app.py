8a6c5f01c56ae3934125d3a54f3de84810b189f7
BreadOwl <71919494+BreadOwl@users.noreply.github.com>
2022-12-23 22:45:01 +0300

    Add files via upload

--- app2/app/app.py
+++ app2/app/app.py
@@ -4,27 +4,36 @@ import mysql.connector
 import json
 from flask_migrate import Migrate
 from app.models import Book
+from flask import render_template, send_from_directory
+from flask_sqlalchemy import SQLAlchemy
+import os
 migrate = Migrate(app, db)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'hard to guess'
+#UPLOAD_FOLDER = 'uploads'
+#app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+db = SQLAlchemy(app)
 
+from app import models
 
-def test_table() -> List[Dict]:
-    config = {
-        'user': 'root',
-        'password': 'root',
-        'host': 'db',
-        'port': '3306',
-        'database': 'devopsroles'
-    }
-    connection = mysql.connector.connect(**config)
-    cursor = connection.cursor()
-    cursor.execute('SELECT * FROM test_table')
-    results = [{name: color} for (name, color) in cursor]
-    cursor.close()
-    connection.close()
+#def test_table() -> List[Dict]:
+   # config = {
+    #    'user': 'root',
+    #    'password': 'root',
+    #    'host': 'db',
+   #     'port': '3306',
+   #     'database': 'devopsroles'
+   # }
+   # connection = mysql.connector.connect(**config)
+   # cursor = connection.cursor()
+   # cursor.execute('SELECT * FROM test_table')
+    #results = [{name: color} for (name, color) in cursor]
+   # cursor.close()
+   # connection.close()
 
-    return results
+    #return results
 
 
 #@app.route('/')
